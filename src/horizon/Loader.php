67915d01cb1a557c1c6a6772cbc57af6711ff168
yFaithOS <55823530+yFaithOS@users.noreply.github.com>
2019-10-01 13:45:49 -0300

    Update
    apenas um teste

--- src/horizon/Loader.php
+++ src/horizon/Loader.php
@@ -1,3 +1,24 @@
 <?php
 
+namespace horizon;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\utils\Config;
+
+class Loader extends PluginBase {
+    
+    private static $object = null;
+    public $data;
+    
+    public function onLoad() {
+        if($this->getDataFolder()) {
+            @mkdir($this->getDataFolder());
+        }
+        self::$object = $this;
+    }
+
+}
+
 ?>
